58e36b2bfce6ddc07dd4ba1223884418803a212c
anandgitacc <anand.akmail@gmail.com>
2024-05-07 20:37:01 +0530

    sivagit

--- src/org/employee/EmployeeDetails.java
+++ src/org/employee/EmployeeDetails.java
@@ -16,6 +16,10 @@ public class EmployeeDetails { // block or body of the Class
 		System.out.println("Employee mobile : 8939278836");
 	}
 	
+	private void empWorkingStatus() {
+		System.out.println("currently working");
+	}
+	
 	public static void main(String[] args) {
 		
 		// object creation
